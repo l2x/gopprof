772e8618c6d68bd45220d94cc5d796ab220cbd25
leiyonglin <leiyonglin@gmail.com>
2016-07-04 12:21:13 +0000

    update

---
+++ common/event/event.go
@@ -0,0 +1,34 @@
+package event
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+const (
+	EventTypeNode = 0
+)
+
+type EventType int
+
+type Event struct {
+	NodeID string
+	Type   EventType
+	Data   interface{}
+
+	Ack int64
+	Seq int64
+}
+
+var (
+	seq = time.Now().UnixNano()
+)
+
+func NewEvent(nodeID string, typ EventType, data interface{}) *Event {
+	return &Event{
+		NodeID: nodeID,
+		Type:   typ,
+		Data:   data,
+		Seq:    atomic.AddInt64(&seq, 1),
+	}
+}
